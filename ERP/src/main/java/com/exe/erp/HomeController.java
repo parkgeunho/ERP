a5dbb01abd011a7f9e2702215d2f3c2ec926418e
kbl <kbl@KBL-PC>
2016-02-10 16:55:52 +0900

    메인수정이다

--- ERP/src/main/java/com/exe/erp/HomeController.java
+++ ERP/src/main/java/com/exe/erp/HomeController.java
@@ -1,25 +1,15 @@
 package com.exe.erp;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	//ë©”ì¸í™ˆí˜ì´ì§€ 
+	//¸ŞÀÎ¼öÁ¤  
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainboard() {
 		
