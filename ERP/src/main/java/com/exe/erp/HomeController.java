684f8d09f5de393d396dc143a0b447573f1e45ac
Alex <Alex@Alex-PC>
2016-02-10 17:26:06 +0900

    메인 충돌2

--- ERP/src/main/java/com/exe/erp/HomeController.java
+++ ERP/src/main/java/com/exe/erp/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-		//메인 입니다.
+		//메인 입니다. 인데 충돌남
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainboard() {
 		
