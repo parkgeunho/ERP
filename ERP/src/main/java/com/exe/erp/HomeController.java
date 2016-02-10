7b0cd16199750b50cd3300660badcc2e918d3b1e
Alex <Alex@Alex-PC>
2016-02-10 17:41:02 +0900

    asdf

--- ERP/src/main/java/com/exe/erp/HomeController.java
+++ ERP/src/main/java/com/exe/erp/HomeController.java
@@ -9,13 +9,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	
+
+	//메인 입니다. 에러 발생
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String mainboard2() {
+		
+		return "temp";
+		
+	}
 	
 	//메인 입니다. 에러 발생
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainboard() {
 		
-		return "temp";
+		return "temp2";
 		
 	}
 	
