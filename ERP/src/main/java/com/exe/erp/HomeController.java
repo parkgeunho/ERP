2f1c16d54a8ca85e2f9056089366b8a01df021cd
Alex <Alex@Alex-PC>
2016-02-10 17:22:23 +0900

    메인 주석을 다른걸로 변경했습니다.

--- ERP/src/main/java/com/exe/erp/HomeController.java
+++ ERP/src/main/java/com/exe/erp/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-		//메인 주석 한글 변경 했습니다.학교종이 땡
+		//메인 입니다.
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainboard() {
 		
