3cd1bf7ae33584b012c3578c61c9f71ce7f452c1
STU4 <STU4@STU4-PC>
2016-03-07 17:51:42 +0900

    BoardFileController java 추가 BoardController안에 FileController 합치려 했지만 실패 따로 만듦 ERP/pom.xml은 파일 추가 하려 했는데 오류나서 찾아보니 누군가 먼저 해놈

--- ERP/src/main/java/com/exe/board/BoardController.java
+++ ERP/src/main/java/com/exe/board/BoardController.java
@@ -1,25 +1,20 @@
 package com.exe.board;
 
-import java.io.File;
-import java.io.IOException;
+
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.sql.Connection;
-import java.util.List;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -33,6 +28,8 @@ public class BoardController {
 	@Autowired
 	@Qualifier("BoardDAO")
 	BoardDAO dao;
+	
+	
 	
 	@Autowired
 	MyUtil myUtil;
@@ -259,182 +256,15 @@ public class BoardController {
 		  return "redirect:/board/list.action?pageNum=" + pageNum;
 	 
 	  
-	  }
+	  
 	 
+
 	 
-	 //--------------- 파일 ------------------
-	 
-	 
-	  public class BoardFile extends HttpServlet{
-		
-		private static final long serialVersionUID = 1L;
-		  
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-				throws ServletException, IOException {
-			doPost(req, resp);
-		}
-		
-		protected void forward(HttpServletRequest req, HttpServletResponse resp,
-				String url) throws ServletException, IOException {
-			RequestDispatcher rd = req.getRequestDispatcher(url);
-			rd.forward(req, resp);
-		} 
-		
-		@Override
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-				throws ServletException, IOException {
-			
-			req.setCharacterEncoding("UTF-8");
-		
-			Connection conn = DBConn.getConnection();
-			BoardFileDAO dao = new BoardFileDAO(conn);
-		
-			MyUtil myUtil = new MyUtil();
-			
-			String cp = req.getContextPath();
-			String uri = req.getRequestURI();
-			String url;
-			  
-			//파일을 저장할 경로
-			String root = getServletContext().getRealPath("/");
-			String path = root + File.separator + "pds" + 
-								File.separator + "saveFile";
-			
-			File f = new File(path);
-			if(!f.exists())
-				f.mkdirs(); //directory 여러개일경우  ..  s안붙은건 한개만 올리때인데 잘 안쓴다
-			
-			if(uri.indexOf("write.do")!=-1){
-				
-				url = "//write.jsp";
-				forward(req, resp, url);	
-				
-			}else if(uri.indexOf("write_ok.do")!=-1){
-		  
-			//파일 업로드
-			/*String encType = "UTF-8";
-			int maxFileSize = 10*1024*1024;
-				
-			MultipartRequest mr = new MultipartRequest(req,
-					path, maxFileSize, encType,
-					new DefaultFileRenamePolicy());
-		  */
-			//DB에 저장
-			/*if(mr.getFile("upload")!=null){
-				
-				int maxNum = dao.getMaxNum();
-				
-				BoardFileDTO dto = new BoardFileDTO();
-				
-				dto.setBoardNum(maxNum + 1);				
-				dto.setSaveFileName(mr.getFilesystemName("upload"));
-				dto.setOriginalFileName(mr.getOriginalFileName("upload"));
-				
-				dao.insertData(dto);
-				
-			}
-			
-			url = cp + "/file/list.do";
-			resp.sendRedirect(url);
-			  
-		  
-	  }
-	 
-		}else if(uri.indexOf("list.do")!=-1){
-			
-			//첫 페이지에선 필요하지 않고 그 후에 필요
-			String pageNum = req.getParameter("pageNum");
-			
-			//현재 페이지는 1
-			int currentPage = 1;
-			
-			//첫 페이지에선 pageNum 이 널 값이기때문에 패스
-			if(pageNum!=null){
-				currentPage = Integer.parseInt(pageNum);
-			}
-			
-			//전체데이터 갯수
-			int dataCount = dao.getDataCount();
-			
-			//한페이지에 표시할 레코드 갯수
-			int numPerPage = 5;
-			
-			int totalPage = myUtil.getPageCount(numPerPage, dataCount);
-			
-			//삭제를 할 경우 IF: 전체페이지가 표시할 페이지보다 큰 경우
-			if(currentPage>totalPage)
-				currentPage = totalPage;
-			
-			//가져올 rownum의 start와 end값 (공식)
-			int start = (currentPage-1)*numPerPage+1; //예:현재 페이지2, 5개   start = 6
-			int end = currentPage*numPerPage;         //예:                    end = 10
-			
-			List<BoardFileDTO> lists = dao.getList(start, end);
-			
-			///////////////////////////////////////다운로드/////////////////////////////////////
-			////////////////////////파일명에 링크를 거는데 서블릿에선 경로를 넘겨줘야한다//////
-			String downloadPath = cp + "/file/download.do";
-			String deletePath = cp + "/file/delete.do";
-			
-			
-			//이미지 넣기
-			String imagePath = cp + "/pds/saveFile";
-			req.setAttribute("imagePath", imagePath);
-			
-		
-			//페이징 처리
-			String listUrl = "list.do";
-			String pageIndexList = 
-					myUtil.pageIndexList(currentPage, totalPage, listUrl);
-			
-			req.setAttribute("lists", lists);
-			req.setAttribute("dataCount", dataCount);
-			req.setAttribute("pageIndexList", pageIndexList);
-			req.setAttribute("downloadPath", downloadPath);
-			req.setAttribute("deletePath", deletePath);
-			
-			url = "/fileTest/list.jsp";
-			forward(req, resp, url);
-			
-		}else if(uri.indexOf("delete.do")!=-1){
-			
-			int BoardNum = Integer.parseInt(req.getParameter("BoardNum")); //Integer.parseInt 숫자로 바꿔줌 "num"문자값을 num숫자 형태로
-			
-			BoardFileDTO dto = dao.getReadData(BoardNum);
-			
-			//파일삭제
-			FileManager.doFileDelete(dto.getSaveFileName(), path);
-									
-			//DB삭제
-			dao.deleteData(BoardNum);			
-					
-			url = cp + "/file/list.do";
-			resp.sendRedirect(url);
-			
-			
-			//파일 다운로드
-		}else if(uri.indexOf("download.do")!=-1){
-						
-			int BoardNum = Integer.parseInt(req.getParameter("BoardNum")); //
-			
-			BoardFileDTO dto = dao.getReadData(BoardNum); //한 사람의 데이터
-			
-			if(dto==null)
-				return;
-	 
-	 
-	 
-	 
-	 */
-		}
-	  }
-	 
-	 
-	 
-	 
+	
 	 
 	  }
+	 
+	  
 	 
 
 }
