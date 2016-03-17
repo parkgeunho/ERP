a2172e21a6cef8da3b405ddc3f3c27beaab10706
STU15 <STU15@STU15-PC>
2016-03-17 17:00:03 +0900

    파일추가

--- ERP/src/main/java/com/exe/board/BoardController.java
+++ ERP/src/main/java/com/exe/board/BoardController.java
@@ -248,7 +248,15 @@ public class BoardController {
 			request.setAttribute("parent", parent);
 			request.setAttribute("boardlist", boardlist);
 			  
-			 
+			
+			
+
+		  return "boardMain";
+	  }
+	  @RequestMapping(value = "/ajaxBoardList", method = {RequestMethod.GET,RequestMethod.POST})
+		public String ajaxBoardList(HttpServletRequest request,HttpServletResponse response) throws UnsupportedEncodingException {
+		  
+		  
 			String cp = request.getContextPath();
 		      
 		      String pageNum = request.getParameter("pageNum");
@@ -256,7 +264,7 @@ public class BoardController {
 		      
 		      String num = request.getParameter("listNum");
 			  	
-			  	System.out.println("num을 확인합니다" + num);
+			  
 		      
 		      
 		      if(pageNum != null)
@@ -298,14 +306,9 @@ public class BoardController {
 		      
 		      List<BoardDTO> lists = dao.getListTest(start, end, searchKey, searchValue, listNum);
 		      
-		      Iterator<BoardDTO> test = lists.iterator();
-		      
-		      while(test.hasNext()){
-		    	  BoardDTO testdto = test.next();
-		    	  System.out.println("번호확인" + testdto.getName());
-		    	  
-		      }
-		      
+		
+		      ListDTO lDTO = listDAO.readData(listNum);
+		     
 		      
 		      String param = "";
 		      
@@ -327,15 +330,20 @@ public class BoardController {
 		      if(!param.equals(""))
 		         articleUrl = articleUrl + "&" + param;
 		      
-		      request.setAttribute("listNum", listNum);
+		      request.setAttribute("LDTO", lDTO);
 		      request.setAttribute("lists", lists);
 		      request.setAttribute("pageIndexList", pageIndexList);
 		      request.setAttribute("dataCount", dataCount);
 		      request.setAttribute("articleUrl", articleUrl);
-			
-
-		  return "boardMain";
+		      
+		      return "board/list";
+		  
 	  }
+		  
+		  
+	  
+	  
+	  
 	  
 
 	  
