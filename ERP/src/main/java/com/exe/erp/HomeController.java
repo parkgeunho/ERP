211d1769eede1d2a43a04f59c4af5fa982f8410d
STU15 <STU15@STU15-PC>
2016-03-21 14:56:19 +0900

    alert창 뜨는거 수정 관리자 모드에서 게시판 관리쪽 디자인 수정중

--- ERP/src/main/java/com/exe/erp/HomeController.java
+++ ERP/src/main/java/com/exe/erp/HomeController.java
@@ -1,24 +1,41 @@
 package com.exe.erp;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.exe.board.BoardDAO;
+import com.exe.board.BoardDTO;
 import com.exe.member.MemberDTO;
 
 @Controller
 public class HomeController {
 
+	
+	
+	@Autowired
+	@Qualifier("boardDAO")
+	BoardDAO BoardDAO;
+	
 	//메인 홈페이지 이동
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainboard(HttpServletRequest request,HttpServletResponse response) {
 		HttpSession session = request.getSession();
 		MemberDTO LoginDTO = (MemberDTO)session.getAttribute("dto");
 		request.setAttribute("LoginDTO", LoginDTO);
+		
+		List<BoardDTO> lists = BoardDAO.getNotice();
+		
+		request.setAttribute("notice", lists);
+		
 		return "temp";
 	}	
 	
