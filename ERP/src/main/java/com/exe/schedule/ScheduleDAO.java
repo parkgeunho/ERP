6c5ab7a1c2f5de621443728a180122dab1060bfa
Alex <Alex@Alex-PC>
2016-03-25 10:20:45 +0900

    왜이럼 ㅋㅋㅋㅋㅋㅋㅋ

---
+++ ERP/src/main/java/com/exe/schedule/ScheduleDAO.java
@@ -0,0 +1,92 @@
+package com.exe.schedule;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.exe.board.BoardDTO;
+
+public class ScheduleDAO {
+	
+	private SqlSessionTemplate sessionTemplate;
+	   
+	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
+		this.sessionTemplate = sessionTemplate;
+	}
+	
+	public int getMaxNum(){
+	      
+		int result = 0;
+
+		result = sessionTemplate.selectOne("ScheduleMapper.maxNum");
+	      
+		return result;
+	}
+	
+	public void insertSchedule(ScheduleDTO dto){
+	      
+		sessionTemplate.insert("ScheduleMapper.insertData",dto);
+	}
+	
+	public List<ScheduleDTO> getList(String id){
+	      
+		List<ScheduleDTO> lists = sessionTemplate.selectList("ScheduleMapper.getLists", id);
+	      
+		return lists;
+	}
+	   
+	   //전체 데이터수 구하기
+	public int getDataCount(String id){
+	      
+		int result = 0;
+	      
+		result = sessionTemplate.selectOne("ScheduleMapper.getDataCount",id);
+	      
+		return result;
+	}
+	
+	public ScheduleDTO getReadData(String scheduleNum){
+	      
+		ScheduleDTO dto = sessionTemplate.selectOne("ScheduleMapper.getReadData", scheduleNum);
+	      
+		return dto;
+	}
+
+	public void updateData(ScheduleDTO dto){
+
+		sessionTemplate.update("ScheduleMapper.updateData",dto);
+	}
+
+	public void dropData(ScheduleDTO dto){
+
+		sessionTemplate.update("ScheduleMapper.dropData",dto);
+	}
+
+	public void deleteData(String ScheduleNum){
+
+		sessionTemplate.delete("ScheduleMapper.deleteData",ScheduleNum);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ERP/src/main/java/com/exe/schedule/ScheduleDTO.java
@@ -0,0 +1,77 @@
+package com.exe.schedule;
+
+public class ScheduleDTO {
+	
+	private int scheduleNum;
+	private String id;
+	private String title;
+	private String content;
+	private String created;
+	private String startDate;
+	private String endDate;
+	private String startTime;
+	private String endTime;
+	
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	private int listNum;
+	
+	public int getScheduleNum() {
+		return scheduleNum;
+	}
+	public void setScheduleNum(int scheduleNum) {
+		this.scheduleNum = scheduleNum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+}
